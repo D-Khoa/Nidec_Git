758d9e2f96acbc7f6e87963cff0be7d74c3e5e85
NDK <dk.wofts@gmail.com>
2020-03-04 21:48:17 +0700

    finish convert program

--- ConvertLB6/ConvertLB6/Form1.cs
+++ ConvertLB6/ConvertLB6/Form1.cs
@@ -30,42 +30,71 @@ namespace ConvertLB6
 
         private void btnBrowserTo_Click(object sender, EventArgs e)
         {
-            OpenFileDialog opfrm = new OpenFileDialog();
-            opfrm.Filter = "Text File (*.txt)|*.txt|CSV File (*.csv)|*.csv|All File (*.*)|*.*";
-            if (opfrm.ShowDialog() == DialogResult.OK)
+            SaveFileDialog savefrm = new SaveFileDialog();
+            savefrm.CheckFileExists = false;
+            savefrm.CheckPathExists = false;
+            savefrm.Filter = "Text File (*.txt)|*.txt|CSV File (*.csv)|*.csv|All File (*.*)|*.*";
+            if (savefrm.ShowDialog() == DialogResult.OK)
             {
-                txtFileTo.Text = opfrm.FileName;
+                txtFileTo.Text = savefrm.FileName;
             }
         }
 
         private void btnOk_Click(object sender, EventArgs e)
         {
-            int n = 0;
-            bool firstline = true;
-            datafile dataf = new datafile();
-            List<datafile> listData = new List<datafile>();
-            foreach (string line in File.ReadLines(txtFileSource.Text))
+            try
             {
-                if (!string.IsNullOrEmpty(line) && n == 0)
+                int n = 0;
+                string cols = string.Empty;
+                string dataline = string.Empty;
+                List<string> filecontent = new List<string>();
+                cols = "barcode;JUDGE CW;JUDGE CCW;";
+                for (int i = 1; i < 21; i++)
+                    cols += "data " + i + ";";
+                cols += "model" + Environment.NewLine;
+                filecontent.Add(cols);
+                foreach (string line in File.ReadLines(txtFileSource.Text))
                 {
-                    dataf.serno = line;
-                    n++;
+                    if (!string.IsNullOrEmpty(line) && !line.Contains("<DATA>") && n < 24)
+                    {
+                        if (!string.IsNullOrEmpty(line) && n == 0)
+                        {
+                            dataline += line.Trim() + ";";                  //0
+                        }
+                        if (line.Contains("JUDGE CW"))
+                        {
+                            string temp = line.Replace(" ", string.Empty);  //1
+                            temp = temp.Split('=')[1];
+                            dataline += temp + ";";
+                        }
+                        if (line.Contains("JUDGE CCW"))                     //2
+                        {
+                            string temp = line.Replace(" ", string.Empty);
+                            temp = temp.Split('=')[1];
+                            dataline += temp + ";";
+                        }
+                        if (line.Contains(",") && (n > 2))                  //3~23
+                        {
+                            string temp = line.Split(',')[0];
+                            dataline += temp + ";";
+                        }
+                        n++;
+                    }
+                    if (n == 24)                                            //24
+                    {
+                        string temp = line.Trim();
+                        dataline += temp + Environment.NewLine;
+                        filecontent.Add(dataline);
+                        dataline = string.Empty;
+                        n = 0;
+                    }
                 }
-                if (line.Contains("JUDGE CW"))
-                {
-                    dataf.judgeCW = line.Split('=')[1].Trim();
-                    n++;
-                }
-                if (line.Contains("JUDGE CCW"))
-                {
-                    dataf.judgeCW = line.Split('=')[1].Trim();
-                    n++;
-                }
-                if (!line.Contains("<DATA>") && !firstline)
-                {
-
-                }
-
+                File.WriteAllLines(txtFileTo.Text, filecontent);
+                MessageBox.Show("Convert completed!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
 
@@ -73,33 +102,5 @@ namespace ConvertLB6
         {
             Application.Exit();
         }
-    }
-
-    public class datafile
-    {
-        public string serno { get; set; }
-        public string judgeCW { get; set; }
-        public string judgeCCW { get; set; }
-        public string col1 { get; set; }
-        public string col2 { get; set; }
-        public string col3 { get; set; }
-        public string col4 { get; set; }
-        public string col5 { get; set; }
-        public string col6 { get; set; }
-        public string col7 { get; set; }
-        public string col8 { get; set; }
-        public string col9 { get; set; }
-        public string col10 { get; set; }
-        public string col11 { get; set; }
-        public string col12 { get; set; }
-        public string col13 { get; set; }
-        public string col14 { get; set; }
-        public string col15 { get; set; }
-        public string col16 { get; set; }
-        public string col17 { get; set; }
-        public string col18 { get; set; }
-        public string col19 { get; set; }
-        public string col20 { get; set; }
-        public string model { get; set; }
     }
 }
