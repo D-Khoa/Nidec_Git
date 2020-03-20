c0a0d182559a0594f1347455dfd329aa7c523456
Diep Nguyen <nguyendiepspkt@gmail.com>
2020-03-20 16:06:55 +0700

    fix iqc

--- New Model Checking Result/NewModelCheckingResult/NewModelCheckingResult/View/DiagaugeForm/DiagaugeForm.cs
+++ New Model Checking Result/NewModelCheckingResult/NewModelCheckingResult/View/DiagaugeForm/DiagaugeForm.cs
@@ -16,6 +16,28 @@ namespace NewModelCheckingResult.View.DiagaugeForm
         public DiagaugeForm()
         {
             InitializeComponent();
+            tcDiagauge.ItemSize = new Size(0, 1);
+        }
+
+        private void DiagaugeForm_Load(object sender, EventArgs e)
+        {
+            getComboListFromDB(ref cmbModel);
+
+        }
+        public void getComboListFromDB(ref ComboBox cmb)
+        {
+            string sql_model = "select model from tbl_model_dbplace order by model";
+            System.Diagnostics.Debug.Print(sql_model);
+            TfSQL tf = new TfSQL();
+            tf.getComboBoxData(sql_model, ref cmb);
+
+            if (cmbModel.Items.Count > 0)
+                cmbModel.SelectedIndex = 0;
+        }
+
+        private void btnEditMaster_Click(object sender, EventArgs e)
+        {
+            tcDiagauge.SelectedTab = tabData;
         }
     }
 }
