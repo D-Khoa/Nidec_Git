2334c08344db41651ad3c6f5065eb84d1585edce
DiepNguyen <nguyendiepspkt@gmail.com>
2020-06-09 11:48:38 +0700

    add LBT

---
+++ LBT52/Scale LBT52/Scale LBT52/Form1.Designer.cs
@@ -0,0 +1,398 @@
+﻿namespace Scale_LBT52
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtkq = new System.Windows.Forms.TextBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbStopBit = new System.Windows.Forms.ComboBox();
+            this.cmbParity = new System.Windows.Forms.ComboBox();
+            this.cmbDataBits = new System.Windows.Forms.ComboBox();
+            this.cmbRate = new System.Windows.Forms.ComboBox();
+            this.cmbCom = new System.Windows.Forms.ComboBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.status = new System.Windows.Forms.ToolStripStatusLabel();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Datetime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.txtkq);
+            this.panel1.Controls.Add(this.btnExport);
+            this.panel1.Controls.Add(this.dgvData);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(823, 512);
+            this.panel1.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(296, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Values:";
+            // 
+            // txtkq
+            // 
+            this.txtkq.Location = new System.Drawing.Point(287, 45);
+            this.txtkq.Multiline = true;
+            this.txtkq.Name = "txtkq";
+            this.txtkq.Size = new System.Drawing.Size(125, 212);
+            this.txtkq.TabIndex = 20;
+            this.txtkq.TextChanged += new System.EventHandler(this.txtkq_TextChanged);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExport.ForeColor = System.Drawing.Color.Black;
+            this.btnExport.Location = new System.Drawing.Point(31, 428);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(167, 47);
+            this.btnExport.TabIndex = 19;
+            this.btnExport.Text = "Export Excel";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // dgvData
+            // 
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Datetime});
+            this.dgvData.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dgvData.Location = new System.Drawing.Point(428, 0);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.Size = new System.Drawing.Size(395, 512);
+            this.dgvData.TabIndex = 18;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnClear);
+            this.groupBox2.Controls.Add(this.btnClose);
+            this.groupBox2.Controls.Add(this.btnDisconnect);
+            this.groupBox2.Controls.Add(this.btnConnect);
+            this.groupBox2.Location = new System.Drawing.Point(12, 297);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(202, 99);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.ForeColor = System.Drawing.Color.Blue;
+            this.btnClear.Location = new System.Drawing.Point(19, 66);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(74, 27);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.ForeColor = System.Drawing.Color.Fuchsia;
+            this.btnClose.Location = new System.Drawing.Point(112, 66);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(74, 27);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.ForeColor = System.Drawing.Color.Red;
+            this.btnDisconnect.Location = new System.Drawing.Point(112, 20);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(74, 27);
+            this.btnDisconnect.TabIndex = 6;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnConnect.Location = new System.Drawing.Point(19, 20);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(74, 27);
+            this.btnConnect.TabIndex = 5;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cmbStopBit);
+            this.groupBox1.Controls.Add(this.cmbParity);
+            this.groupBox1.Controls.Add(this.cmbDataBits);
+            this.groupBox1.Controls.Add(this.cmbRate);
+            this.groupBox1.Controls.Add(this.cmbCom);
+            this.groupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.groupBox1.Location = new System.Drawing.Point(12, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(206, 251);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tool Control:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Blue;
+            this.label5.Location = new System.Drawing.Point(16, 216);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Stop Bit";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(16, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Parity";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(16, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Data Bits";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(16, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Baud Rate";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(16, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COM";
+            // 
+            // cmbStopBit
+            // 
+            this.cmbStopBit.ForeColor = System.Drawing.Color.Red;
+            this.cmbStopBit.FormattingEnabled = true;
+            this.cmbStopBit.Location = new System.Drawing.Point(79, 206);
+            this.cmbStopBit.Name = "cmbStopBit";
+            this.cmbStopBit.Size = new System.Drawing.Size(100, 21);
+            this.cmbStopBit.TabIndex = 4;
+            this.cmbStopBit.SelectedIndexChanged += new System.EventHandler(this.cmbStopBit_SelectedIndexChanged);
+            // 
+            // cmbParity
+            // 
+            this.cmbParity.ForeColor = System.Drawing.Color.Red;
+            this.cmbParity.FormattingEnabled = true;
+            this.cmbParity.Location = new System.Drawing.Point(79, 159);
+            this.cmbParity.Name = "cmbParity";
+            this.cmbParity.Size = new System.Drawing.Size(100, 21);
+            this.cmbParity.TabIndex = 3;
+            this.cmbParity.SelectedIndexChanged += new System.EventHandler(this.cmbParity_SelectedIndexChanged_1);
+            // 
+            // cmbDataBits
+            // 
+            this.cmbDataBits.ForeColor = System.Drawing.Color.Red;
+            this.cmbDataBits.FormattingEnabled = true;
+            this.cmbDataBits.Location = new System.Drawing.Point(79, 118);
+            this.cmbDataBits.Name = "cmbDataBits";
+            this.cmbDataBits.Size = new System.Drawing.Size(100, 21);
+            this.cmbDataBits.TabIndex = 2;
+            this.cmbDataBits.SelectedIndexChanged += new System.EventHandler(this.cmbDataBits_SelectedIndexChanged);
+            // 
+            // cmbRate
+            // 
+            this.cmbRate.ForeColor = System.Drawing.Color.Red;
+            this.cmbRate.FormattingEnabled = true;
+            this.cmbRate.Location = new System.Drawing.Point(79, 78);
+            this.cmbRate.Name = "cmbRate";
+            this.cmbRate.Size = new System.Drawing.Size(100, 21);
+            this.cmbRate.TabIndex = 1;
+            this.cmbRate.SelectedIndexChanged += new System.EventHandler(this.cmbRate_SelectedIndexChanged_1);
+            // 
+            // cmbCom
+            // 
+            this.cmbCom.ForeColor = System.Drawing.Color.Red;
+            this.cmbCom.FormattingEnabled = true;
+            this.cmbCom.Location = new System.Drawing.Point(79, 39);
+            this.cmbCom.Name = "cmbCom";
+            this.cmbCom.Size = new System.Drawing.Size(100, 21);
+            this.cmbCom.TabIndex = 0;
+            this.cmbCom.SelectedIndexChanged += new System.EventHandler(this.cmbCom_SelectedIndexChanged_1);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.status});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 515);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(823, 22);
+            this.statusStrip1.TabIndex = 17;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // status
+            // 
+            this.status.ForeColor = System.Drawing.Color.Red;
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(73, 17);
+            this.status.Text = "StatusLabel1";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "No";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 30;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Values";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 110;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Unit";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 70;
+            // 
+            // Datetime
+            // 
+            this.Datetime.HeaderText = "DateTime";
+            this.Datetime.Name = "Datetime";
+            this.Datetime.Width = 140;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 537);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Scale LBT82";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbStopBit;
+        private System.Windows.Forms.ComboBox cmbParity;
+        private System.Windows.Forms.ComboBox cmbDataBits;
+        private System.Windows.Forms.ComboBox cmbRate;
+        private System.Windows.Forms.ComboBox cmbCom;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel status;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.TextBox txtkq;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Datetime;
+    }
+}
+
---
+++ LBT52/Scale LBT52/Scale LBT52/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace Scale_LBT52
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt;
+        SerialPort P = new SerialPort(); // Khai báo 1 Object SerialPort mới.
+        string InputData = String.Empty; // Khai báo string buff dùng cho hiển thị dữ liệu sau này.
+        delegate void SetTextCallback(string text);
+        public Form1()
+        {
+            InitializeComponent();
+            string[] ports = SerialPort.GetPortNames();
+
+            cmbCom.Items.AddRange(ports);
+            P.ReadTimeout = 1000;
+            P.DataReceived += new SerialDataReceivedEventHandler(DataReceive);
+            string[] BaudRate = { "2400" };
+            cmbRate.Items.AddRange(BaudRate);
+            string[] Databits = { "7" };
+            cmbDataBits.Items.AddRange(Databits);
+            string[] Parity = { "Even" };
+            cmbParity.Items.AddRange(Parity);
+            string[] stopbit = { "1" };
+            cmbStopBit.Items.AddRange(stopbit);
+            dt = new DataTable();
+            dt.Columns.Add("no");
+            dt.Columns.Add("value");
+            dt.Columns.Add("unit");
+            dt.Columns.Add("datetime");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //cmbCom.SelectedIndex = 0;
+            //cmbRate.SelectedIndex = 3; // 9600
+            //cmbDataBits.SelectedIndex = 2; // 8
+            //cmbParity.SelectedIndex = 0; // None
+            //cmbStopBit.SelectedIndex = 0; // None
+            // Hiện thị Status cho Pro tí
+            status.Text = "Please choose port com to continue!!";
+
+        }
+
+
+        private void DataReceive(object obj, SerialDataReceivedEventArgs e)
+        {
+            InputData = P.ReadExisting();
+            if (InputData != String.Empty)
+            {
+                SetText(InputData);
+            }
+        }
+        private void SetText(string text)
+        {
+            if (this.txtkq.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else this.txtkq.Text += text;
+        }
+
+
+        private void cmbCom_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (P.IsOpen)
+            {
+                P.Close();
+            }
+            P.PortName = cmbCom.SelectedItem.ToString();
+        }
+
+        private void cmbRate_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (P.IsOpen)
+            {
+                P.Close();
+            }
+            P.BaudRate = Convert.ToInt32(cmbRate.Text);
+        }
+
+        private void cmbDataBits_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (P.IsOpen)
+            {
+                P.Close();
+            }
+            P.DataBits = Convert.ToInt32(cmbDataBits.Text);
+        }
+
+        private void cmbParity_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            if (P.IsOpen)
+            {
+                P.Close();
+            }
+            switch (cmbParity.SelectedItem.ToString())
+            {
+                case "Odd":
+                    P.Parity = Parity.Odd;
+                    break;
+                case "None":
+                    P.Parity = Parity.None;
+                    break;
+                case "Even":
+                    P.Parity = Parity.Even;
+                    break;
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                P.Open();
+                btnDisconnect.Enabled = true;
+                btnConnect.Enabled = false;
+                status.Text = "Connected to Port: " + cmbCom.SelectedItem.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No Connect", "Try Again", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            P.Close();
+            btnConnect.Enabled = true;
+            btnDisconnect.Enabled = false;
+            // Hiện thị Status
+            status.Text = "Disconnect";
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cmbStopBit_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (P.IsOpen)
+            {
+                P.Close();
+            }
+            switch (cmbStopBit.SelectedItem.ToString())
+            {
+                case "1":
+                    P.StopBits = StopBits.One;
+                    break;
+                case "1.5":
+                    P.StopBits = StopBits.OnePointFive;
+                    break;
+                case "2":
+                    P.StopBits = StopBits.Two;
+                    break;
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                SaveFileDialog savef = new SaveFileDialog();
+                savef.Filter = "Excel Workbook (*.xlsx)|*.xlsx|Excel 97-2003 Workbook (*.xls)|*.xls|All file (*.*)|*.*";
+                savef.AddExtension = true;
+                if (savef.ShowDialog() == DialogResult.OK)
+                {
+                    ExcelClass excel = new ExcelClass(savef.FileName);
+                    excel.CreateWorkBook();
+                    excel.AddDatatable(dt);
+                    excel.SaveAndExit();
+                    MessageBox.Show("Export Successfully", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else return;
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+
+        }
+
+
+
+        private void txtkq_TextChanged(object sender, EventArgs e)
+        {
+            if (txtkq.Text.Contains("\r\n"))
+            {
+                string qtys = txtkq.Text;
+                string[] arrListStr = qtys.Split(' ');
+                int i = dt.Rows.Count;
+                DataRow dr = dt.NewRow();
+
+                if (arrListStr[1] == "")
+                {
+                    arrListStr[1] = "g";
+                }
+                dr["no"] = i;
+                dr["value"] = arrListStr[0].Remove(0, 3);
+                dr["unit"] = arrListStr[1];
+                dr["datetime"] = DateTime.Now.ToString();
+                dt.Rows.Add(dr);
+                string firstColumn = i++.ToString();
+                string secondColumn = txtkq.Text;
+                string[] row = { firstColumn, arrListStr[0].Remove(0, 3), arrListStr[1], DateTime.Now.ToString() };
+                dgvData.Rows.Add(row);
+                txtkq.Text = null;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            dgvData.Rows.Clear();
+            dt.Clear();
+            txtkq.Clear();
+        }
+    }
+
+
+}
