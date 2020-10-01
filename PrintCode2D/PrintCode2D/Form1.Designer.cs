75cb2a8c00db1a84bc99ca9fb332fbd1f5d561d6
DiepNguyen <nguyendiepspkt@gmail.com>
2020-10-01 15:44:51 +0700

    add print code 2d

---
+++ PrintCode2D/PrintCode2D/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace PrintCode2D
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCustomerCode = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSerinumber = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBoxCode = new System.Windows.Forms.GroupBox();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.dtpMonth = new System.Windows.Forms.DateTimePicker();
+            this.dtpyear = new System.Windows.Forms.DateTimePicker();
+            this.btnEditCode = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.groupBoxCode.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer Code:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Year:";
+            // 
+            // txtCustomerCode
+            // 
+            this.txtCustomerCode.Location = new System.Drawing.Point(111, 25);
+            this.txtCustomerCode.Name = "txtCustomerCode";
+            this.txtCustomerCode.Size = new System.Drawing.Size(83, 20);
+            this.txtCustomerCode.TabIndex = 2;
+            this.txtCustomerCode.Text = "TLR2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Month:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Date:";
+            // 
+            // txtSerinumber
+            // 
+            this.txtSerinumber.Location = new System.Drawing.Point(111, 157);
+            this.txtSerinumber.Name = "txtSerinumber";
+            this.txtSerinumber.Size = new System.Drawing.Size(83, 20);
+            this.txtSerinumber.TabIndex = 9;
+            this.txtSerinumber.Text = "00001";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Serinumber:";
+            // 
+            // groupBoxCode
+            // 
+            this.groupBoxCode.Controls.Add(this.dtpDate);
+            this.groupBoxCode.Controls.Add(this.dtpMonth);
+            this.groupBoxCode.Controls.Add(this.dtpyear);
+            this.groupBoxCode.Controls.Add(this.label1);
+            this.groupBoxCode.Controls.Add(this.txtSerinumber);
+            this.groupBoxCode.Controls.Add(this.label2);
+            this.groupBoxCode.Controls.Add(this.label5);
+            this.groupBoxCode.Controls.Add(this.txtCustomerCode);
+            this.groupBoxCode.Controls.Add(this.label4);
+            this.groupBoxCode.Controls.Add(this.label3);
+            this.groupBoxCode.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxCode.Name = "groupBoxCode";
+            this.groupBoxCode.Size = new System.Drawing.Size(243, 207);
+            this.groupBoxCode.TabIndex = 10;
+            this.groupBoxCode.TabStop = false;
+            this.groupBoxCode.Text = "2D Code Setting";
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.CustomFormat = "dd";
+            this.dtpDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpDate.Location = new System.Drawing.Point(111, 121);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(83, 20);
+            this.dtpDate.TabIndex = 12;
+            // 
+            // dtpMonth
+            // 
+            this.dtpMonth.CustomFormat = "MM";
+            this.dtpMonth.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpMonth.Location = new System.Drawing.Point(111, 88);
+            this.dtpMonth.Name = "dtpMonth";
+            this.dtpMonth.Size = new System.Drawing.Size(83, 20);
+            this.dtpMonth.TabIndex = 11;
+            // 
+            // dtpyear
+            // 
+            this.dtpyear.CustomFormat = "yyyy";
+            this.dtpyear.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpyear.Location = new System.Drawing.Point(111, 55);
+            this.dtpyear.Name = "dtpyear";
+            this.dtpyear.Size = new System.Drawing.Size(83, 20);
+            this.dtpyear.TabIndex = 10;
+            // 
+            // btnEditCode
+            // 
+            this.btnEditCode.Location = new System.Drawing.Point(12, 225);
+            this.btnEditCode.Name = "btnEditCode";
+            this.btnEditCode.Size = new System.Drawing.Size(103, 35);
+            this.btnEditCode.TabIndex = 12;
+            this.btnEditCode.Text = "Edit Code";
+            this.btnEditCode.UseVisualStyleBackColor = true;
+            this.btnEditCode.Click += new System.EventHandler(this.btnEditCode_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(134, 225);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(103, 35);
+            this.btnPrint.TabIndex = 13;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(134, 276);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(103, 35);
+            this.btnExit.TabIndex = 16;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(12, 276);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(103, 35);
+            this.btnReset.TabIndex = 17;
+            this.btnReset.Text = "Reset Label";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 335);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnEditCode);
+            this.Controls.Add(this.groupBoxCode);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "BMD-0088 Print Label";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBoxCode.ResumeLayout(false);
+            this.groupBoxCode.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCustomerCode;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSerinumber;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBoxCode;
+        private System.Windows.Forms.Button btnEditCode;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.DateTimePicker dtpMonth;
+        private System.Windows.Forms.DateTimePicker dtpyear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ PrintCode2D/PrintCode2D/TfPrint.cs
@@ -0,0 +1,110 @@
+﻿namespace PrintCode2D
+{
+    public class TfPrint
+    {
+        public static string printerName = "SEWOO Label Printer";
+
+        public static void openPrinter()
+        {
+            long rtn;
+            /* 1. LK_OpenPrinter() */
+            if (LKBPRINT.LK_OpenPrinter(printerName) != LKBPRINT.LK_SUCCESS)
+                throw new System.Exception("Can't open printer!");
+
+            /* 2. LK_SetupPrinter() */
+            rtn = LKBPRINT.LK_SetupPrinter("70",   // 10~104 (Unit is mm)
+                            "30",       // 5~350 (Unit is mm)
+                            0,              // 0=Label with Gap, 1=Label with Black Mark, 2=Label with Continuous.
+                            "3",            // if(MediaType==0) <GapHeight> else <BlackMarkHeight>. (Unit is mm)
+                            "0",            // if(MediaType==0) <not used> else <distance from BlackMark to perforation>. (Unit is mm)
+                            8,              // 0 ~ 15
+                            6,              // 2 ~ 6 (Unit is Inch)
+                            1               // 1 ~ 9999 copies
+                            );
+
+            if (rtn != LKBPRINT.LK_SUCCESS)
+            {
+                LKBPRINT.LK_ClosePrinter();
+                throw new System.Exception("Can't setup printer");
+                //return;
+            }
+        }
+        public static void closePrinter()
+        {
+            /* 4. LK_ClosePrinter() */
+            LKBPRINT.LK_ClosePrinter();
+        }
+
+        public static void printBarCodeNew(string cus, string year, string month, string date, string seri)
+        {
+            //itemNo = "A32-164D-X";
+            //itemName = "SHAFT";
+            //supplier = "SHINDENSHA CO.,LTD.";
+            //invoice = "NDCV160129";
+            //date = "2017/12/31";
+            //qty = "2500";
+            //validity = string.Empty; //"2017/12/31";
+
+            int x, y;
+            int cell = 6;
+            //string printerName = "SEWOO Label Printer";
+
+            int xdots, model; // ydots;
+            string TwoBAR_Command;
+            string QRCode_data = cus + year + month + date + seri;
+
+            /* 3-1. page 1 test */
+            LKBPRINT.LK_StartPage();
+
+            // QR Code のプリントアウト
+            // bx,y,Q,1,z,L,"DATA"
+            // x = x position.
+            // y = y position
+            // z = Cell Size. (2 ~ 16)
+            // L = ECC Level(L or M or Q or H)
+            // line barcode
+            x = 8 * cell; //36
+            y = 14 * cell; //84
+            xdots = 4; //3
+            model = 2;
+            TwoBAR_Command = string.Format("b{0},{1},Q,{2},{3},L,\"{4}\"\r\n", x, y, model, xdots, QRCode_data);
+            LKBPRINT.LK_DirectCommand(TwoBAR_Command);
+
+
+            ////LINE 1 - S3
+            //x = 20 * cell;
+            //y = 6 * cell;//Y=4
+            //LKBPRINT.LK_PrintDeviceFont(x, y, 0, 3, 1, 1, 0, Asset_No);
+
+            ////LINE 2 - S4
+
+            //x = 20 * cell;
+            //y = 11 * cell; //Y=8
+            //LKBPRINT.LK_PrintDeviceFont(x, y, 0, 3, 1, 1, 0, "Name: " + Asset_Name);
+            ////LINE 3 - S2
+            //x = 20 * cell;
+            //y = 16 * cell;//Y=13
+            //LKBPRINT.LK_PrintDeviceFont(x, y, 0, 3, 1, 1, 0, "Model: " + Model);
+
+            ////LINE 4 - S4
+            //x = 20 * cell;
+            //y = 21 * cell;//Y=17
+            //LKBPRINT.LK_PrintDeviceFont(x, y, 0, 3, 1, 1, 0, "Ser: " + Ser);
+
+            //LINE 5 - S2
+            //x = 20 * cell;
+            //y = 26 * cell;//Y=22
+            // Line SN
+            x = 24 * cell;
+            y = 16 * cell;//Y=22
+                          // LKBPRINT.LK_PrintDeviceFont(x, y, 0, 3, 1, 1, 0, "SN: " + QRCode_data)
+            LKBPRINT.LK_PrintDeviceFont(x, y, 0, 3, 1, 1, 0, "SN: ");
+            //Line data
+            x = 24 * cell;
+            y = 21 * cell;//Y=22
+                          // LKBPRINT.LK_PrintDeviceFont(x, y, 0, 3, 1, 1, 0, "SN: " + QRCode_data)
+            LKBPRINT.LK_PrintDeviceFont(x, y, 0, 3, 1, 1, 0, QRCode_data);
+            LKBPRINT.LK_EndPage();
+        }
+    }
+}
